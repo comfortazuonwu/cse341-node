7ba02d44a5ff7082598fcb7d01cedb3de4f622d0
Comfort Azuonwu <82896331+comfortazuonwu@users.noreply.github.com>
2022-03-01 10:27:49 -0700

    WEEK7

--- app.js
+++ app.js
@@ -7,14 +7,13 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
-require('dotenv').config();
-const PORT = process.env.PORT || 3000;
-
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI =
+  'mongodb+srv://garnet:Ngozimine17@mongostarter.vg8hy.mongodb.net/myFirstDatabase';
+
 const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
@@ -43,26 +42,34 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
-  if (!req.session.user) {
-    return next();
-  }
-  User.findById(req.session.user._id)
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
-});
-
-app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
   res.locals.csrfToken = req.csrfToken();
   next();
 });
 
+app.use((req, res, next) => {
+  // throw new Error('Sync Dummy');
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      next(new Error(err));
+    });
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
+
+app.get('/500', errorController.get500);
 
 app.use(errorController.get404);
 
@@ -71,19 +78,21 @@ const cors0ptions = {
   optionsSuccessStatus:200
 };
 app.use(cors(cors0ptions));
- 
-const options = {
-  // useUnifiedTopology: true,
-  useNewUrlParser: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false,
-  family: 4
-};
+
+app.use((error, req, res, next) => {
+  // res.status(error.httpStatusCode).render(...);
+  // res.redirect('/500');
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
+});
 
 mongoose
-  .connect(MONGODB_URI, options)
+  .connect(MONGODB_URI)
   .then(result => {
-    app.listen(PORT);
+    app.listen(3000);
   })
   .catch(err => {
     console.log(err);
